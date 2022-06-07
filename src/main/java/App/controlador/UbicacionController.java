11f938ccee2d2c1874c4419a6df31b60860aa2c9
al386180 <al386180@uji.es>
2022-06-07 22:40:53 +0200

    corregido un error

--- src/main/java/App/controlador/UbicacionController.java
+++ src/main/java/App/controlador/UbicacionController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.ws.rs.PathParam;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
